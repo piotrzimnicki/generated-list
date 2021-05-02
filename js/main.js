841e1f4f395867b6fe79ca6b839d34300ec642d9
Piotr Zimnicki <ziemniakit@gmail.com>
2021-05-02 13:16:37 +0200

    add style to last item in list

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ createList(8);
 
 function styleAndText () {
     let lastLi = document.querySelector('ul li:last-child')
-    lastLi.classList.add('new-class');
+    lastLi.classList.style.cssText = "color:#000;font-weight: 600;";
     lastLi.textContent = "Ostatni element";
 }
 
